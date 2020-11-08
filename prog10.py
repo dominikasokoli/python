9bd73a492d82d7928b3e3a3c42ea2d5853ee9928
dominikasokoli <dominikasokoli@users.noreply.github.com>
2020-11-08 14:39:09 +0100

    V2

--- prog10.py
+++ prog10.py
@@ -4,14 +4,20 @@ produkt = {'SS12': {'nazwa': 'sukienka_trojka', 'rozmiary': ['s', 'l', 'xl']},
 
 for id in produkt:
     p = produkt[id]
-    print(p)
-    print(p['nazwa'], ['rozmiary'])
+    rozmiary = p['rozmiary']
+    #print(p)
+    #print(rozmiary)
+    print(p['nazwa'])
+    for r in rozmiary:
+        print(r)
 
-statusy = {'Konta' : {'obecny_status': 'Aktywny', 'poprzedni_status': 'Pauza'},
-            'Subskrypcji' : {'obecny_status_sub': 'Aktywny', 'poprzedni_status_sub': 'Aktywny'}
-            }
 
-for id in statusy:
-    s = statusy[id]
-    print(s)
-    print(s['obecny_status'], ['obecny_status_sub'])
+
+#statusy = {'Konta' : {'obecny_status': 'Aktywny', 'poprzedni_status': 'Pauza'},
+#            'Subskrypcji' : {'obecny_status_sub': 'Aktywny', 'poprzedni_status_sub': 'Aktywny'}
+#            }
+
+#for id in statusy:
+#    s = statusy[id]
+#    print(s)
+#    print(s['obecny_status'], ['obecny_status_sub'])
