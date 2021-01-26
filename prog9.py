39a35b89ccea85dfd144c0596618fa0ad1d6270f
dominikasokoli <74092217+dominikasokoli@users.noreply.github.com>
2021-01-26 19:31:05 +0100

    Delete prog9.py

--- prog9.py
+++
@@ -1,9 +0,0 @@
-name = input('podaj nazwe pizzy')
-pizza = {'size': 'medium',
-        'price': 20,
-        'delivery': 2}
-for key, value in pizza.items():
-    print("{1}".format(key, value))
-
-pizza['nazwa'] = name
-print(pizza)
